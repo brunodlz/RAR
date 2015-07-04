3f6693b8b3dd72eeaa0e00102efa9e187a7dc25a
brn0s <brunolabx@gmail.com>
2015-07-03 23:56:44 -0300

    Get Dir

--- app/src/main/java/betweenbits/rar/MainActivity.java
+++ app/src/main/java/betweenbits/rar/MainActivity.java
@@ -4,26 +4,69 @@ import android.app.ListActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends ListActivity {
+
+    private List<String> item = null;
+    private List<String> path = null;
+
+    private String root="/";
+    private TextView myPath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
+    private void getDir(String dirPath)
+
+    {
+
+        myPath.setText("Location: " + dirPath);
+
+        item = new ArrayList<>();
+        path = new ArrayList<String>();
+
+        File f = new File(dirPath);
+        File[] files = f.listFiles();
+
+        if(!dirPath.equals(root)) {
+            item.add(root);
+            path.add(root);
+
+            item.add("../");
+            path.add(f.getParent());
+        }
+
+        for(int i=0; i < files.length; i++) {
+            File file = files[i];
+            path.add(file.getPath());
+
+            if(file.isDirectory()) {
+                item.add(file.getName() + "/");
+            } else {
+                item.add(file.getName());
+            }
+        }
+
+        ArrayAdapter<String> fileList = new ArrayAdapter<String>(this, R.layout.row, item);
+        setListAdapter(fileList);
+    }
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
