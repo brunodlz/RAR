aff7ad60bdf6dd6c5f485468477eb79b4a3b9afc
brn0s <brunolabx@gmail.com>
2015-07-06 23:18:25 -0300

    Add Icons and BaseAdapter

---
+++ app/src/main/java/betweenbits/rar/CustomAdapter.java
@@ -0,0 +1,42 @@
+package betweenbits.rar;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+import java.util.ArrayList;
+
+/**
+ * Created by bruno v0id on 7/6/15.
+ */
+public class CustomAdapter extends BaseAdapter {
+
+    private Context context;
+    private ArrayList<Item> lista;
+
+    public CustomAdapter(Context context, ArrayList<Item> lista) {
+        this.context = context;
+        this.lista = lista;
+    }
+
+    @Override
+    public int getCount() {
+        return lista.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return lista.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return null;
+    }
+}
--- app/src/main/java/betweenbits/rar/MainActivity.java
+++ app/src/main/java/betweenbits/rar/MainActivity.java
@@ -1,68 +1,23 @@
 package betweenbits.rar;
 
-import android.app.ListActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.ArrayAdapter;
 
-import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
-
-public class MainActivity extends ListActivity {
-
-    private List<String> item = null;
-    private List<String> path = null;
-
-    private String root="/";
-    private TextView myPath;
+public class MainActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
-    }
-
-    private void getDir(String dirPath)
-
-    {
-
-        myPath.setText("Location: " + dirPath);
-
-        item = new ArrayList<>();
-        path = new ArrayList<String>();
-
-        File f = new File(dirPath);
-        File[] files = f.listFiles();
-
-        if(!dirPath.equals(root)) {
-            item.add(root);
-            path.add(root);
-
-            item.add("../");
-            path.add(f.getParent());
-        }
-
-        for(int i=0; i < files.length; i++) {
-            File file = files[i];
-            path.add(file.getPath());
-
-            if(file.isDirectory()) {
-                item.add(file.getName() + "/");
-            } else {
-                item.add(file.getName());
-            }
-        }
-
-        ArrayAdapter<String> fileList = new ArrayAdapter<String>(this, R.layout.row, item);
-        setListAdapter(fileList);
     }
 
     @Override
