de2e35caf3ba45439d3ab4eab9af32d0b5dd1c37
Bruno v0id <brunolabx@gmail.com>
2015-07-14 18:04:04 -0300

    Definido apenas portrait.

--- app/src/main/java/betweenbits/rar/CustomAdapter.java
+++ app/src/main/java/betweenbits/rar/CustomAdapter.java
@@ -48,9 +48,6 @@ public class CustomAdapter extends BaseAdapter {
         TextView textTitle = (TextView) layout.findViewById(R.id.textTitleFolder);
         textTitle.setText(item.getTitulo());
 
-//        TextView textDateModify = (TextView) layout.findViewById(R.id.textDateModify);
-//        textDateModify.setText(item.getDataModificacao());
-
         return layout;
     }
 }
